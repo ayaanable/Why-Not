3f1df1c8fe0cd8d6e867d50c102e0e6acf893dcb
Ayaan Singh <ayaankumar2024@lpu.in>
2024-10-31 15:52:49 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+
+function clickYesButton() {
+    alert('I love you more <3');
+}
+
+function hoverNoButton() {
+
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+
+    document.getElementById('NO').style.left = x + 'px';
+    document.getElementById('NO').style.top = y + 'px';
+}
